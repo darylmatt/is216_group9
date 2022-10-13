893f31ffd6f7b029b5e78cf532989795aa428af9
Christian Koh <ckoh.2021@scis.smu.edu.sg>
2022-10-14 00:10:24 +0800

    health landing

--- Health is Wealth/Mental Health/mental_health.js
+++ Health is Wealth/Mental Health/mental_health.js
@@ -1,4 +1,4 @@
-var mental_cards = document.getElementsByClassName("mental-card");
+// var mental_cards = document.getElementsByClassName("mental-card");
 var mental_card_1 = document.getElementById("mental-card-1");
 var mental_card_2 = document.getElementById("mental-card-2");
 var mental_card_3 = document.getElementById("mental-card-3");
@@ -17,40 +17,40 @@ var curr_enlarge3 = false;
 function change_IMG1() {
     if (curr_enlarge1 == false) {
         var curr_img = mental_card_1.getElementsByTagName("img")[0];
-        curr_img.style.width = "110%"
+        curr_img.style.width = "120%"
         curr_enlarge1 = true;
     }
     else{
         var curr_img = mental_card_1.getElementsByTagName("img")[0];
-        curr_img.style.width = "auto"
+        curr_img.style.width = "100%"
         curr_enlarge1 = false;
     }
 }
 
 function change_IMG2() {
 
-    if (curr_enlarge1 == false) {
+    if (curr_enlarge2 == false) {
         var curr_img = mental_card_2.getElementsByTagName("img")[0];
-        curr_img.style.width = "110%"
-        curr_enlarge1 = true;
+        curr_img.style.width = "120%"
+        curr_enlarge2 = true;
     }
     else{
         var curr_img = mental_card_2.getElementsByTagName("img")[0];
-        curr_img.style.width = "auto"
-        curr_enlarge1 = false;
+        curr_img.style.width = "100%"
+        curr_enlarge2 = false;
     }
 }
 
 function change_IMG3() {
 
-    if (curr_enlarge1 == false) {
+    if (curr_enlarge3 == false) {
         var curr_img = mental_card_3.getElementsByTagName("img")[0];
-        curr_img.style.width = "110%"
-        curr_enlarge1 = true;
+        curr_img.style.width = "120%"
+        curr_enlarge3 = true;
     }
     else{
         var curr_img = mental_card_3.getElementsByTagName("img")[0];
-        curr_img.style.width = "auto"
-        curr_enlarge1 = false;
+        curr_img.style.width = "100%"
+        curr_enlarge3 = false;
     }
 }
