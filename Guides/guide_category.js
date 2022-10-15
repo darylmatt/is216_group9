ded654448aeab100f2d05c98f16ebca2e754ef49
Joon Xiang Yong <jxyong.2021@scis.smu.edu.sg>
2022-10-15 16:19:21 +0800

    Create guide_category.js

---
+++ Guides/guide_category.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://wad2g9:flannelgang@cluster0.dxjblb1.mongodb.net/test";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+ 
+   dbo.collection("guides").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+  
+});
