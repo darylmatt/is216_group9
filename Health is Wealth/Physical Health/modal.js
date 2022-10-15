e05ae2885fa2994e70dfb5d0d92bb386b4c7d77b
Daryl Ang <daryl.ang.2021@scis.smu.edu.sg>
2022-10-15 21:55:20 +0800

    maps

--- Health is Wealth/Physical Health/modal.js
+++ Health is Wealth/Physical Health/modal.js
@@ -1,10 +1,7 @@
 
-
-
 function fillTitle(id){
     var h1Div = document.getElementById('myModal').children[0].children[0].children[0].children[0];
     var h1Text = id.toUpperCase();
 
     h1Div.innerText = h1Text
-    console.log(h1Text)
 }
