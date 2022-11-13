7e80cad5a462cb2047ca2c20f947bc3c1ce9fdaf
Joon Xiang Yong <jxyong.2021@scis.smu.edu.sg>
2022-11-13 18:55:01 +0800

    Updated forum

--- scripts/forum.js
+++ scripts/forum.js
@@ -5,7 +5,7 @@ const root = Vue.createApp({
     // Data Properties
     data() {
         return { 
-            threads: "",
+            threads: [],
         }
     },
 
@@ -13,14 +13,26 @@ const root = Vue.createApp({
         axios.get("http://localhost:3000/Social/Forum/forum.html").then( response => { 
             this.threads = JSON.parse(response.headers.threads);
         }
-         );
+         ); 
     }, 
     // Methods
     methods: {
 
         // YOUR CODE GOES HERE IF YOU NEED ANY
         
-        
+        fillModal(id){
+
+            for(let index=0; index < this.threads.length; index++){
+                if(this.threads[index].thread_id == id){
+                    document.getElementById('modalTitle').innerText = this.threads[index].thread_title
+                    document.getElementById('threadContent').innerText = this.threads[index].thread_content
+                    document.getElementById('threadInfo').innerText = 'Thread Author: ' + this.threads[index].author_name
+
+
+
+                }
+            }
+        }
 
     }
     // Other stuff
