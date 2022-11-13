4e88ac1607923763b1d924d8af31a90492792e6c
Christian Koh <ckoh.2021@scis.smu.edu.sg>
2022-11-13 18:24:51 +0800

    login change

---
+++ scripts/login_validate.js
@@ -0,0 +1,13 @@
+var user = 
+    {
+        email : "",
+        password : ""
+    }
+
+var login_check = false;
+
+function validateLogin(){
+    var email = document.getElementById("form3Example3").value;
+    var password = document.getElementById("form3Example4").value;
+    console.log(email, password);
+}
