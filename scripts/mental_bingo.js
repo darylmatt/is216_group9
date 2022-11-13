5f63ca587b5f13bc4abccb69d7a54007be0884ed
Christian Koh <ckoh.2021@scis.smu.edu.sg>
2022-11-13 12:49:27 +0800

    mental_bingo

---
+++ scripts/mental_bingo.js
@@ -0,0 +1,32 @@
+var bingo_data = 
+{
+    "Go out in the sun" : "../../img/bingo/sun.png",
+    "Take a walk" : "../../img/bingo/walking.png",
+    "Meditate" : "../../img/bingo/meditation.png",
+    "Dress Up" : "../../img/bingo/dressup.png",
+    "Take a breather" : "../../img/bingo/breathing.png",
+    "Read a book" : "../../img/bingo/book.png",
+    "Self Affirmation" : "../../img/bingo/self-esteem.png",
+    "See a film" : "../../img/bingo/watching-a-movie.png",
+    "Watch a funny video" : "../../img/bingo/video.png",
+    "Meet friends" : "../../img/bingo/friends.png",
+    "Eat good food" : "../../img/bingo/hot-pot.png",
+    "Journal" : "../../img/bingo/journal.png",
+    "A Relaxing Bath" : "../../img/bingo/bathtub.png",
+    "Call a friend" : "../../img/bingo/video-call.png",
+    "Go to the park" : "../../img/bingo/park.png",
+    "Go for a run" : "../../img/bingo/running.png"
+}
+
+function initialiseBoard() {
+    
+}
+var random_int = (Math.floor(Math.random) * 16) + 1;
+
+console.log(random_int);
+// function generateOrder(){
+//     var return_arr = [];
+//     while (return_arr.length < 16) {
+//         var random_int = (Math.floor(Math.random) * 16) + 1
+//     }
+// }
